17a25c0a215b8d59354dc51fc74643024061ee31
Derik300309 <derik3009@gmail.com>
2020-11-29 10:28:42 +0500

    final version

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
 var swiper = new Swiper('.swiper-container', {
-  effect: 'coverflow',
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: 'auto',
+  effect: 'cube',
   loop: true,
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-  },
+      grabCursor: true,
+      cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+      },
   pagination: {
     el: '.swiper-pagination',
   },
