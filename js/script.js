dcd75e22564db49bedc98c2b13f3f5eaca7e472f
Derik300309 <derik3009@gmail.com>
2020-11-29 10:19:31 +0500

    the final version

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
-var mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  direction: 'horizontal',
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
   loop: true,
-
-  // If we need pagination
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
   pagination: {
     el: '.swiper-pagination',
   },
-
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-})
+});
